2a4e79bc79728c0c152b106de82904c000c31d57
Joyce Hong <jkim2027@gmail.com>
2023-03-06 14:45:56 -0500

    Update friend.py

---
+++ friend.py
@@ -0,0 +1,11 @@
+from mysqlconnection import connectToMySQL
+
+class Friend:
+    def __init__(self, data):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.occupation = data['occupation']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+    
